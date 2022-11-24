d6c7666a08829955cfd848d77f90fe192474b80a
RuslanPervakov <115935852+RuslanPervakov@users.noreply.github.com>
2022-11-24 18:40:59 +0500

    Update first_tests.py

--- tests/first_tests.py
+++ tests/first_tests.py
@@ -2,20 +2,20 @@ import pytest
 from app.calculator import Calculator
 
 class TestCalc:
-    def setup(self):                      #
-        self.calc = Calculator            
+    def setup(self):                     
+        self.calc = Calculator         
 
-    def test_adding_calculate_correctly(self):
+    def test_addition_calculate_correctly(self):
         assert self.calc.adding(self, 7, 8) == 15
 
     def test_subtraction_calculate_correctly(self):
         assert self.calc.subtraction(self, 4, 3) == 1
 
-    def test_multiply_calculate_correctly(self):      
-        assert self.calc.multiply(self, 2, 2) == 4    
+    def test_multiplication_calculate_correctly(self):      
+        assert self.calc.multiplication(self, 2, 2) == 4    
 
     def test_division_calculate_correctly(self):
         assert  self.calc.division(self, 8, 4) == 2
 
-    def test_multiply_calculation_failed(self):       
-        assert self.calc.multiply(self, 2, 2) == 5    
+    def test_multiplication_calculation_failed(self):       
+        assert self.calc.multiplication(self, 2, 2) == 7          
